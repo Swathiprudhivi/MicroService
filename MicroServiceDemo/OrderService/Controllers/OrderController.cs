ca08467e4cf1328b21ec97eef1b6a59a1f36a67c
Swathiprudhivi <prudhiviswathi@gmail.com>
2020-02-21 01:26:02 -0800

    21/02/2020

---
+++ MicroServiceDemo/OrderService/Controllers/OrderController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+using OrderService.Repositories;
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderRepository _repo;
+        public OrderController(IOrderRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_repo.GetAll());
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.InnerException.Message);
+            }
+        }
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(string id)
+        {
+            try
+            {
+                return Ok(_repo.GetById(id));
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.InnerException.Message);
+            }
+        }
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult POST(Orders order)
+        {
+            try
+            {
+                _repo.AddOrder(order);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.InnerException.Message);
+            }
+        }
+        [HttpPut]
+        [Route("UpadateOrder")]
+        public IActionResult PUT(Orders order)
+        {
+            try
+            {
+                _repo.UpdateOrder(order);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.InnerException.Message);
+            }
+        }
+        [HttpDelete]
+        [Route("DeleteOrder/{id}")]
+        public IActionResult Delete(string id)
+        {
+            try
+            {
+                _repo.DeleteOrder(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.InnerException.Message);
+            }
+        }
+    }
+}
